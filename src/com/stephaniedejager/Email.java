814403d7d8c20caad14f2ef22a892084acb3ef59
Stephanie de Jager <sdejager.za@gmail.com>
2022-05-05 14:41:43 +0200

    Create Blueprint for email class

--- src/com/stephaniedejager/Email.java
+++ src/com/stephaniedejager/Email.java
@@ -1,2 +1,21 @@
 public class Email {
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //Constructor receives the first name and last name
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set mailbox capacity
+
+    //Set alternate email
+
+    //Set password
+
 }
